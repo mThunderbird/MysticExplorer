1f457f3be8c97d26bfdaee8a5da835b221b516fe
Dragostin Ivanov <dragostin.ivanov@gmail.com>
2021-08-09 15:39:08 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+#include "SDL2\include\SDL.h"
+
+#undef main
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	SDL_Rect test;
+	
+	test = { 100 , 100 ,100 ,100 }; 
+
+	cout << "hello world";
+
+	return 0;
+}
